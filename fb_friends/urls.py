aaf0256dace4e9964587d53bd8e95a3d4a41b84a
Ira <you@example.com>
2017-11-07 14:39:26 +0300

    fix insert to userfriends

--- fb_friends/urls.py
+++ fb_friends/urls.py
@@ -24,11 +24,9 @@ def after_user_signed_up(sender, request, **kwargs):
 
     user_id = kwargs['sociallogin'].account.user_id
     user_friends = kwargs['sociallogin'].account.extra_data['friends']['data']
-    obj_for_user = UserFriends.objects.filter(user_id=user_id)
 
     for user_friend in user_friends:
-        if not obj_for_user.filter(friend_id=int(user_friend['id'])):
-            obj = UserFriends(user_id=user_id,
+        obj = UserFriends.objects.get_or_create(user_id=user_id,
                               friend_id=int(user_friend['id']), friend_name=user_friend['name'])
-            obj.save()
+
 
